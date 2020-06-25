ebd2ac9640af3d517414efec90276734e04a1ab7
Anne de Joly <anne.dejoly@edhec.com>
2020-06-25 11:51:19 +0100

    Basic style'

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    margin: 'auto',
+    flexGrow: 1,
+    spacing: 8,
+    backgroundColor: '#292932',
+    color: '#FFFFFF',
+  },
+});
 
 function Banner({ restaurant }) {
+  const classes = useStyles();
   const { id, name, tags, city, description } = restaurant;
+  console.log('Restaurant: ', restaurant);
 
   return (
-    <Box key={id}>
-      <h1>{name}</h1>
-      <h3>{tags.map((tag) => tag.name + ' ')}</h3>
-      <h3>{city}</h3>
+    <Box className={classes.root} position="static" key={id}>
+      <div>
+        <h1>{name}</h1>
+        <h3>{tags.map((tag) => tag.name + ' ')}</h3>
+        <h3>{city}</h3>
+      </div>
       <p>{description}</p>
     </Box>
   );
